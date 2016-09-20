baccbfc99f6c04a377c5537cda8d88d38ebd6d7b
MistaMase <jjjmase123@gmail.com>
2016-09-19 22:31:48 -0700

    Add files via upload

---
+++ Sep2016/Homework12.12/MehMain.java
@@ -0,0 +1,39 @@
+import java.io.FileNotFoundException;
+
+public class MehMain 
+{
+	public static void main(String []args) throws FileNotFoundException
+	{
+		java.util.Scanner inputSC = new java.util.Scanner(System.in);
+		System.out.println("Please enter the name of the file you wish to change");
+		String fileInput = inputSC.nextLine();
+		java.io.File in = new java.io.File(fileInput);
+		java.util.Scanner sc = new java.util.Scanner(in);
+		java.io.File out = new java.io.File("ThisJavaFile.java");
+		java.io.PrintWriter output = new java.io.PrintWriter(out);
+
+
+		boolean firstTime = true;
+		while(sc.hasNextLine())
+		{
+			String tempString = sc.nextLine();
+			if(tempString.contains("{"))
+			{
+				output.print(" {");
+			}
+			else if(tempString.length() == 0) {}
+			else if(firstTime)
+				output.print(tempString);
+			else
+			{
+				output.println();
+				output.print(tempString);
+			}
+			firstTime = false;
+		}
+		
+		inputSC.close();
+		sc.close();
+		output.close();
+	}
+}
